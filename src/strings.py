8bdc52ad84e2ef1ca074ac92ccd508d503e2f721
Marc-Olivier Buob <marc-olivier.buob@nokia-bell-labs.com>
2019-01-18 00:59:30 +0100

    Added missing module

--- src/strings.py
+++ src/strings.py
@@ -10,7 +10,7 @@ __email__      = "marc-olivier.buob@nokia-bell-labs.com"
 __copyright__  = "Copyright (C) 2018, Nokia"
 __license__    = "BSD-3"
 
-import re, string
+import re, string, unicodedata
 
 MAP_TO_INTERNATIONAL = {
     # Upper case

b419067e1145e750dcb38b06bbaf1b4e724cad23
Marc-Olivier Buob <marc-olivier.buob@nokia-bell-labs.com>
2019-02-26 17:30:21 +0100

    Added union

---
+++ src/union.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# This file is part of the minifold project.
+# https://github.com/nokia/minifold
+
+__author__     = "Marc-Olivier Buob"
+__maintainer__ = "Marc-Olivier Buob"
+__email__      = "marc-olivier.buob@nokia-bell-labs.com"
+__copyright__  = "Copyright (C) 2018, Nokia"
+__license__    = "BSD-3"
+
+from itertools          import chain
+from minifold.connector import Connector
+from minifold.query     import Query, ACTION_READ
+
+def union_gen(list_entries :list):
+    """
+    Iterator over a list of list of dicts.
+    Args:
+        list_entries: A list of lists of dicts.
+    Returns:
+        The resulting list of dicts
+
+    """
+    return (entry for entry in chain(*list_entries))
+
+def union(list_entries :list):
+    """
+    Make the union of several lists of entries.
+    Use minifold.distinct() to remove duplicates.
+    Args:
+        list_entries: A list of lists of dicts.
+    Returns:
+        The resulting list of dicts
+    """
+    return [entry for entry in union_gen(list_entries)]
+
+class UnionConnector(Connector):
+    def __init__(self, childs :list):
+        self.m_childs = childs
+
+    def attributes(self, object :str) -> set:
+        attrs = set()
+        for child in self.m_childs:
+            try:
+                attrs |= child.attributes(object)
+            except:
+                pass
+        return attrs
+
+    def query(self, query :Query) -> list:
+        super().query(query)
+        if query.action != ACTION_READ:
+            raise ValueError("Invalid action in query %s" % query)
+        return union([child.query(query) for child in self.m_childs])
+
---
+++ tests/test_union.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env pytest-3
+# -*- coding: utf-8 -*-
+#
+# Authors:
+#   Marc-Olivier Buob <marc-olivier.buob@nokia-bell-labs.com>
+
+from minifold.count             import CountConnector, count
+from minifold.entries_connector import EntriesConnector
+from minifold.query             import Query
+from minifold.union             import UnionConnector, union
+
+ENTRIES = [
+    {'a' : 1,   'b' : 2,   'c' : 3},
+    {'a' : 10,  'b' : 20,  'c' : 30},
+    {'a' : 100, 'b' : 200, 'c' : 300},
+    {'a' : 100, 'b' : 200, 'd' : 400},
+]
+
+ENTRIES_CONNECTOR = EntriesConnector(ENTRIES)
+EMPTY_CONNECTOR = EntriesConnector([])
+
+def test_union():
+    assert union([ENTRIES, []]) == ENTRIES
+    assert union([[], ENTRIES]) == ENTRIES
+    assert union([ENTRIES, ENTRIES]) == ENTRIES + ENTRIES
+
+def test_union_connector_empty():
+    union_connector = UnionConnector([ENTRIES_CONNECTOR, EMPTY_CONNECTOR])
+    union_connector.query(Query()) == ENTRIES
+
+def test_union_connector_duplicates():
+    union_connector = UnionConnector([ENTRIES_CONNECTOR, ENTRIES_CONNECTOR])
+    union_connector.query(Query()) == ENTRIES + ENTRIES
+
+

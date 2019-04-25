8d1b49227f6a7c9c8eb68560759658a8514202fb
Marc-Olivier Buob <marc-olivier.buob@nokia-bell-labs.com>
2019-04-25 17:45:33 +0200

    Fixed LambdasConnector behavior to not leak attributes not explicitely queried. Added lambdas test suite

--- src/lambdas.py
+++ src/lambdas.py
@@ -10,8 +10,26 @@ __email__      = "marc-olivier.buob@nokia-bell-labs.com"
 __copyright__  = "Copyright (C) 2018, Nokia"
 __license__    = "BSD-3"
 
-from .connector             import Connector
-from .query                 import Query, ACTION_READ
+from copy       import deepcopy
+from .connector import Connector
+from .query     import Query
+
+def find_lambda_dependencies(func) -> set:
+    entry = dict()
+    while True:
+        try:
+            val = func(entry)
+        except KeyError as e:
+            needed_key = e.args[0]
+            entry[needed_key] = None
+            continue
+        except Exception as e:
+            pass
+        break
+    return set(entry.keys())
+
+def find_lambdas_dependencies(map_lambdas :dict) -> dict:
+    return {attr : find_lambda_dependencies(func) for attr, func in map_lambdas.items()}
 
 def lambdas(map_lambdas :dict, entries :list, attributes :set = None) -> list:
     attrs = set(map_lambdas.keys())
@@ -27,32 +45,60 @@ def lambdas(map_lambdas :dict, entries :list, attributes :set = None) -> list:
     return entries
 
 class LambdasConnector(Connector):
-    def __init__(self, map_lambdas :dict, child):
+    def __init__(self, map_lambdas :dict, child :Connector, map_dependencies :dict = None):
         super().__init__()
         self.m_child = child
         self.m_map_lambdas = map_lambdas
 
+        # If no attribute dependency is provided, use find_lambda_dependencies heuristic.
+        self.m_map_dependencies = map_dependencies \
+            if map_dependencies is not None \
+            else find_lambdas_dependencies(map_lambdas)
+
     @property
-    def child(self):
+    def child(self) -> Connector:
         return self.m_child
 
     def attributes(self, object :str) -> set:
         return self.m_child.attributes(object) | set(self.m_map_lambdas.keys())
 
-    def query(self, q :Query) -> list:
-        #matching_lambdas_keys = set(q.attributes) & set(self.m_map_lambdas.keys())
-        #if len(matching_lambdas_keys):
-        #    Log.warning("LambdasConnector::query: attributes in %s won't be fetched" % matching_lambdas_keys)
+    def find_needed_attributes(self, attributes :set) -> set:
+        ret = set()
+        for attribute in attributes:
+            needed_attributes = self.m_map_dependencies.get(attribute, set([attribute]))
+            ret |= needed_attributes
+        return ret
 
-        # TODO :
-        # - if q.attributes involves a field in m_map_lambdas.keys(), select(q.attributes, self.child(SELECT * ...))
-        # - if q.filters    involves a field in m_map_lambdas.keys(), where(q.filters, self.child(... WHERE True))
+    def query(self, q :Query) -> list:
         super().query(q)
-        return self.answer(
+        q_child = deepcopy(q)
+
+        if q.attributes or q.filters:
+            where_attributes = self.find_needed_attributes(find_lambda_dependencies(q.filters))
+
+            if q.attributes:
+                select_attributes  = self.find_needed_attributes(set(q.attributes))
+                lambdas_attributes = set(self.m_map_lambdas.keys())
+                q_child.attributes = list((select_attributes | where_attributes) - lambdas_attributes)
+
+            if where_attributes:
+                # If the filter involves at least one attribute of the LambdasConnector
+                # the filter is not forwarded to the child node, and the filtering is
+                # achieved locally achieved locally to this LambdasConnector.
+                # As a sequel, the child node may return entries that could be filtered
+                # thanks to the self.reshape_entries method.
+                q_child.filters = None
+
+        child_entries = self.child.query(q_child)
+        entries = self.answer(
             q,
             lambdas(
                 self.m_map_lambdas,
-                self.child.query(q),
+                child_entries,
                 q.attributes
             )
         )
+        print("%s: entries = %s" % (self, child_entries))
+        ret = self.reshape_entries(q, entries)
+        print("%s: q = %s reshaped_entries = %s" % (self, q, ret))
+        return ret
---
+++ tests/test_lambdas.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env pytest-3
+# -*- coding: utf-8 -*-
+#
+# This file is part of the minifold project.
+# https://github.com/nokia/minifold
+
+__author__     = "Marc-Olivier Buob"
+__maintainer__ = "Marc-Olivier Buob"
+__email__      = "marc-olivier.buob@nokia-bell-labs.com"
+__copyright__  = "Copyright (C) 2018, Nokia"
+__license__    = "BSD-3"
+
+from minifold.entries_connector import EntriesConnector
+from minifold.query             import Query
+from minifold.lambdas           import LambdasConnector, find_lambda_dependencies, find_lambdas_dependencies
+from minifold.log               import Log
+
+Log.enable_print = True
+
+ENTRIES = [
+    {"a" : 1,   "b" : 2,   "c" : 3},
+    {"a" : 10,  "b" : 20,  "c" : 30},
+    {"a" : 100, "b" : 200, "c" : 300}
+]
+
+class StrictEntriesConnector(EntriesConnector):
+    def __init__(self, entries):
+        super().__init__(entries)
+        self.last_queried_attributes = None
+
+    def query(self, q :Query) -> list:
+        assert set(q.attributes) <= self.attributes(None)
+        self.last_queried_attributes = set(q.attributes)
+        return super().query(q)
+
+STRICT_CONNECTOR = StrictEntriesConnector(ENTRIES)
+
+MAP_LAMBDAS = {
+    "a2" : lambda e: e["a"] ** 2,       # a^2
+    "a3" : lambda e: e["a"] * e["a2"],  # a^3
+    "b2" : lambda e: e["b"] ** 2,       # ...
+    "b3" : lambda e: e["b"] * e["b2"],
+    "c2" : lambda e: e["c"] ** 2,
+    "c3" : lambda e: e["c"] * e["c2"],
+}
+
+LAMBDAS_CONNECTOR = LambdasConnector(MAP_LAMBDAS, STRICT_CONNECTOR)
+
+def check_keys(entries :list, expected_keys :set):
+    for entry in entries:
+        assert set(entry.keys()) == expected_keys
+
+def test_find_lambda_dependencies():
+    obtained = find_lambda_dependencies(MAP_LAMBDAS["a2"])
+    assert obtained == {"a"}
+    obtained = find_lambda_dependencies(MAP_LAMBDAS["a3"])
+    assert obtained == {"a", "a2"}
+
+def test_find_lambdas_dependencies():
+    map_required_keys = find_lambdas_dependencies(MAP_LAMBDAS)
+    expected = {
+        "a2" : {"a"},
+        "a3" : {"a", "a2"},
+        "b2" : {"b"},
+        "b3" : {"b", "b2"},
+        "c2" : {"c"},
+        "c3" : {"c", "c2"},
+    }
+    #Log.debug(map_required_keys)
+    #Log.debug(expected)
+    assert map_required_keys == expected
+
+def test_lambdas_simple():
+    attributes = {"a", "b", "c"}
+    obtained = LAMBDAS_CONNECTOR.query(Query(attributes = attributes))
+    check_keys(obtained, attributes)
+    assert obtained == ENTRIES
+
+def test_lambdas_all():
+    attributes = {
+        "a", "a2", "a3",
+        "b", "b2", "b3",
+        "c", "c2", "c3",
+    }
+
+    obtained = LAMBDAS_CONNECTOR.query(Query())
+    check_keys(obtained, attributes)
+    assert STRICT_CONNECTOR.last_queried_attributes == set()
+
+    obtained = LAMBDAS_CONNECTOR.query(Query(attributes = attributes))
+    check_keys(obtained, attributes)
+    assert STRICT_CONNECTOR.last_queried_attributes == {"a", "b", "c"}
+
+def test_lambdas_x2():
+    attributes = {"a2", "b2", "c2"}
+    obtained = LAMBDAS_CONNECTOR.query(Query(attributes = attributes))
+    check_keys(obtained, attributes)
+
+def test_lambdas_where():
+    expected = [
+        {
+            "a"  : 10,
+            "a2" : 100,
+            "a3" : 1000,
+            "b"  : 20,
+            "b2" : 400,
+            "b3" : 8000,
+            "c"  : 30,
+            "c2" : 900,
+            "c3" : 27000
+        }
+    ]
+
+    obtained = LAMBDAS_CONNECTOR.query(Query(filters = lambda e: e["a"] == 10))
+    assert obtained == expected
+
+    obtained = LAMBDAS_CONNECTOR.query(Query(filters = lambda e: e["a2"] == 100))
+    assert obtained == expected
+
+def test_lambdas_select_where():
+    expected = [
+        {
+            "a2" : 100,
+            "a3" : 1000,
+            "b"  : 20,
+            "b2" : 400,
+        }
+    ]
+
+    obtained = LAMBDAS_CONNECTOR.query(Query(
+        attributes = ["a2", "a3", "b", "b2"],
+        filters = lambda e: e["a"] == 10
+    ))
+    assert obtained == expected
+
+    obtained = LAMBDAS_CONNECTOR.query(Query(
+        attributes = ["a2", "a3", "b", "b2"],
+        filters = lambda e: e["a2"] == 100
+    ))
+    assert obtained == expected
+
+
+def test_lambdas_attributes():
+    obtained = LAMBDAS_CONNECTOR.attributes(None)
+    expected = {
+        "a", "a2", "a3",
+        "b", "b2", "b3",
+        "c", "c2", "c3",
+    }
+    assert obtained == expected
+
